8a577b8055eda69267222a991307f3be1fa37e3f
Erickson <erick2109@gmail.com>
2021-07-16 00:24:41 -0400

    subiendo seed de company

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,58 @@
 if User.find_by_email('admin@sipca.com').nil?
   User.create(email: 'admin@sipca.com', password: '12345678')
 end
+
+coverage_items = [
+  {title: 'Hospitaliacion', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Uic', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Cirugia Electiva', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Cirugia Emergencia', value: 100, pe: nil },
+  {title: 'Maternidad', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Servicios de Emergencias', value: 100, pe: nil },
+  {title: 'Consultas Basicas', value: 100, pe: nil },
+  {title: 'Consultas Especializadas', value: 100, pe: nil },
+  {title: 'Servicios de Odontologia Basica', value: 100, pe: nil },
+  {title: 'Rayos x', value: 100, pe: nil },
+  {title: 'Mamografia', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Tomografia', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Examenes Basicos', value: 100, pe: nil },
+  {title: 'Examenes Lab. Especiales', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Ecogramas Basicos', value: 100, pe: nil },
+  {title: 'Examenes Especiales', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Electrocardiograma', value: 100, pe: nil },
+  {title: 'Ecocardiograma, Mapa, Holter', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Gastrocopia', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Colonoscopia', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Mastologia', value: 0, pe: '(1 Ano P/E)' }
+]
+coverage_items_oro = [
+  {title: 'Hospitaliacion', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Uic', value: 100, pe: '(6 Meses P/E)' },
+  {title: 'Cirugia Electiva', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Cirugia Emergencia', value: 100, pe: nil },
+  {title: 'Maternidad', value: 100, pe: '(1 Ano P/E)' },
+  {title: 'Servicios de Emergencias', value: 100, pe: nil },
+  {title: 'Consultas Basicas', value: 100, pe: nil },
+  {title: 'Consultas Especializadas', value: 100, pe: nil },
+  {title: 'Servicios de Odontologia Basica', value: 100, pe: nil },
+  {title: 'Rayos x', value: 100, pe: nil },
+  {title: 'Mamografia', value: 100, pe: '(6 Meses P/E)' },
+  {title: 'Tomografia', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Examenes Basicos', value: 100, pe: nil },
+  {title: 'Examenes Lab. Especiales', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Ecogramas Basicos', value: 100, pe: nil },
+  {title: 'Examenes Especiales', value: 100, pe: '(3 Meses P/E)' },
+  {title: 'Electrocardiograma', value: 100, pe: nil },
+  {title: 'Ecocardiograma, Mapa, Holter', value: 100, pe: nil },
+  {title: 'Gastrocopia', value: 100, pe: '(6 Meses P/E)' },
+  {title: 'Colonoscopia', value: 100, pe: '(6 Meses P/E)' },
+  {title: 'Mastologia', value: 0, pe: '(1 Ano P/E)' }
+]
+
+unless Company.all.present?
+  company = Company.create({ name: 'SIPCA ', rif: 'J-00000000-0' })
+  producs = company.products.create({ description: 'Medicina Prepagada', title: 'SIPCA' })
+  bronce = producs.plans.create({ coverage: 12000, coverage_items: coverage_items, payment_fee: 18, title: 'Bronce' })
+  plata = producs.plans.create({ coverage: 20000, coverage_items: coverage_items, payment_fee: 25, title: 'Plata' })
+  oro = producs.plans.create({ coverage: 50000, coverage_items: coverage_items_oro, payment_fee: 45, title: 'Oro' })
+end
