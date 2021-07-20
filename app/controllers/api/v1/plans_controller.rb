219b9eb8e0249ff112e64ad375424039595070f7
Erickson <erick2109@gmail.com>
2021-07-19 22:49:01 -0400

    subiendo create para plans

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -3,6 +3,17 @@ class Api::V1::PlansController < ApplicationController
     render json: Plans.all
   end
 
+  def create
+    @plan = Plan.new(plan_params)
+
+    if @plan.save!
+      render json: { message: 'Plan creado con exito', status: 'ok'}
+    else
+      render json: { message: 'Ocurrio un erro al crear el plan',
+        erros: @plan.errors.messages , status: 'fail' }, status: 400 and return
+    end
+  end
+
   def update
     @plan.update(plan_params)
       render json: { message: 'Plan actualizado con exito', status: 'ok' }
